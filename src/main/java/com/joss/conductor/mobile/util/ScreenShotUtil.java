f9a612cd6d688b73c1eb5f484001ce91e92dbe04
Joss <jossjacobo@gmail.com>
2016-11-30 15:44:19 -0500

    Fixed screenshot file creation bug
    I added check for file creation to remove invalid characters and to
    limit the file name length.

--- src/main/java/com/joss/conductor/mobile/util/ScreenShotUtil.java
+++ src/main/java/com/joss/conductor/mobile/util/ScreenShotUtil.java
@@ -1,6 +1,7 @@
 package com.joss.conductor.mobile.util;
 
 import com.joss.conductor.mobile.Locomotive;
+import io.appium.java_client.AppiumDriver;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 
@@ -14,33 +15,55 @@ public class ScreenShotUtil {
 
     private static final String DIR = "/target/test-screenshots";
     private static final String WORKING_DIR = System.getProperty("user.dir");
+    private static final String PNG_EXT = ".png";
 
     public static void take(Locomotive locomotive, String testName) {
+        writeFile(locomotive.driver, createFilePathAndName(testName));
+    }
+
+    public static void take(Locomotive locomotive, String path, String testName) {
+        writeFile(locomotive.driver, createFilePathAndName(path, testName));
+    }
+
+    private static void writeFile(AppiumDriver appiumDriver, String filePathAndName) {
         try {
-            FileUtils.copyFile(locomotive.driver.getScreenshotAs(OutputType.FILE),
-                    new File(WORKING_DIR
-                            + File.separator
-                            + DIR
-                            + File.separator
-                            + testName.replace(File.separator, "-") + ".png"));
+            FileUtils.copyFile(appiumDriver.getScreenshotAs(OutputType.FILE), new File(filePathAndName));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void take(Locomotive locomotive, String path, String testName) {
-        try {
-            FileUtils.copyFile(locomotive.driver.getScreenshotAs(OutputType.FILE),
-                    new File(WORKING_DIR
-                            + File.separator
-                            + DIR
-                            + File.separator
-                            + path.replace(File.separator, "-")
-                            + File.separator
-                            + testName.replace(File.separator, "-") + ".png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private static String createFilePathAndName(String testName) {
+        return WORKING_DIR
+                + File.separator
+                + DIR
+                + File.separator
+                + limitLengthTo100Chars(removeInvalidFilenameChars(testName))
+                + PNG_EXT;
+    }
+
+    private static String createFilePathAndName(String path, String testName) {
+        return WORKING_DIR
+                + File.separator
+                + DIR
+                + File.separator
+                + limitLengthTo100Chars(removeInvalidFilenameChars(path))
+                + File.separator
+                + limitLengthTo100Chars(removeInvalidFilenameChars(testName))
+                + PNG_EXT;
+    }
+
+    private static String removeInvalidFilenameChars(String name) {
+        return name.replace(File.separator, "-")
+                .replace(":", "-")
+                .replace("?", "-")
+                .replace("*", "-")
+                .replace("|", "-")
+                .replace(":", "-");
+    }
+
+    private static String limitLengthTo100Chars(String name) {
+        return name.substring(0, name.length() > 100 ? 100 : name.length());
     }
 }
 
