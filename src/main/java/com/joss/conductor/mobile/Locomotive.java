6952ea99e4348eca1b16bf63c72d16aa2d52e545
ashby-bowles <ashby.bowles@WTA-MBP104-AshbyBowles.local>
2017-09-18 16:16:23 -0400

    Removed swipeTo method in other PR

--- src/main/java/com/joss/conductor/mobile/Locomotive.java
+++ src/main/java/com/joss/conductor/mobile/Locomotive.java
@@ -428,57 +428,6 @@ public class Locomotive extends Watchman implements Conductor<Locomotive> {
 
 
 
-    public WebElement swipeTo(SwipeElementDirection s, By by, int attempts){
-
-        int i;
-
-        if (isPresentWait(by)){
-            return driver.findElement(by);
-        }
-        else {
-            for (i = 0; i < attempts; i++) {
-
-                swipeCenter(s);
-                if (isPresentWait(by)) {
-                    return driver.findElement(by);
-                }
-
-            }
-
-        }
-        System.err.println("WARN: Element" + by.toString() + "does not exist!");
-        return null;
-
-
-    }
-
-
-    public WebElement swipeTo(SwipeElementDirection s, String id, int attempts){
-
-        int i;
-
-        if (isPresentWait(id)){
-            return driver.findElementById(id);
-        }
-        else {
-            for (i = 0; i < attempts; i++) {
-
-                swipeCenter(s);
-                if (isPresentWait(id)) {
-                    return driver.findElementById(id);
-                }
-
-            }
-
-        }
-        System.err.println("WARN: Element" + id.toString() + "does not exist!");
-        return null;
-
-
-    }
-
-
-
     /**
      * Get center point of element, if element is null return center of screen
      *
