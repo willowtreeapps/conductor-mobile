b38fb4b14187ff3aae4e6c4edf8c5ef2bec8f8c1
Andrew Owen <andrew.owen@willowtreeapps.com>
2017-12-06 16:01:28 -0500

    remove unneeded calculations

--- src/main/java/com/joss/conductor/mobile/Locomotive.java
+++ src/main/java/com/joss/conductor/mobile/Locomotive.java
@@ -485,23 +485,25 @@ public class Locomotive extends Watchman implements Conductor<Locomotive> {
     private Locomotive performCornerSwipe(ScreenCorner corner, SwipeElementDirection direction, float percentage, int duration) {
         Dimension screen = driver.manage().window().getSize();
 
+        int MARGINPX_10 = 10;
+
         Point from;
         if(corner != null) {
             int x = 0;
             int y = 0;
             switch(corner) {
                 case TOP_LEFT:
-                    x = screen.getWidth() - (screen.getWidth() - 10);
+                    x = MARGINPX_10;
                     y = screen.getHeight() - (screen.getHeight() - 10);
                     from = new Point(x,y);
                     break;
                 case TOP_RIGHT:
                     x = screen.getWidth() - 10;
-                    y = screen.getHeight() - (screen.getHeight() - 10);
+                    y = MARGINPX_10;
                     from = new Point(x,y);
                     break;
                 case BOTTOM_LEFT:
-                    x = screen.getWidth() - (screen.getWidth() - 10);
+                    x = MARGINPX_10;
                     y = screen.getHeight() - 10;
                     from = new Point(x,y);
                     break;
