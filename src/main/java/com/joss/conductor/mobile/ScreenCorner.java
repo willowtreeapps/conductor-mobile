68b896ef5952363ba6f96f2a2c461f6b32cfe4d3
Andrew Owen <andrew.owen@willowtreeapps.com>
2017-12-06 11:22:02 -0500

    remove extra blank lines from ScreenCorner Enum

--- src/main/java/com/joss/conductor/mobile/ScreenCorner.java
+++ src/main/java/com/joss/conductor/mobile/ScreenCorner.java
@@ -1,19 +1,11 @@
 package com.joss.conductor.mobile;
 
-import org.openqa.selenium.Point;
-
-
 public enum ScreenCorner {
     TOP_LEFT("top left"),
     TOP_RIGHT("top right"),
     BOTTOM_LEFT("bottom left"),
     BOTTOM_RIGHT("bottom right");
 
-
-
-
     String corner;
     ScreenCorner(String corner) { this.corner = corner;}
-
-
 }
