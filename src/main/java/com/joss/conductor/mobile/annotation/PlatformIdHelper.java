01cfebf434d98bacd8fcf460503ad46c55c87773
Matt Hess <matt.hess@willowtreeapps.com>
2018-03-16 13:41:41 -0400

    Loop through superclass

--- src/main/java/com/joss/conductor/mobile/annotation/PlatformIdHelper.java
+++ src/main/java/com/joss/conductor/mobile/annotation/PlatformIdHelper.java
@@ -2,6 +2,7 @@ package com.joss.conductor.mobile.annotation;
 
 import com.joss.conductor.mobile.Platform;
 import org.openqa.selenium.By;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 
@@ -12,34 +13,41 @@ public final class PlatformIdHelper {
 
     public static void initIds(Object page, Platform platform) throws Exception {
 
-        for (Field field : page.getClass().getDeclaredFields()) {
-            for (Annotation annotation : field.getDeclaredAnnotations()) {
-                if (!field.getType().equals(By.class)) {
-                    throw new ClassCastException("Must be of type By! Field name: " + field.getName());
-                }
+        Class current = page.getClass();
+        do {
+            for (Field field : current.getDeclaredFields()) {
+                for (Annotation annotation : field.getDeclaredAnnotations()) {
+                    if (!field.getType().equals(By.class)) {
+                        throw new ClassCastException("Must be of type By! Field name: " + field.getName());
+                    }
 
-                if (annotation instanceof AndroidId) {
-                    AndroidId androidIdAnnotation = (AndroidId) annotation;
-                    if (platform.equals(Platform.ANDROID)) {
-                        if (!androidIdAnnotation.value().isEmpty()) {
-                            field.set(page, By.id(androidIdAnnotation.value()));
-                        } else if (!androidIdAnnotation.xpath().isEmpty()) {
-                            field.set(page, By.xpath(androidIdAnnotation.xpath()));
+                    if (annotation instanceof AndroidId) {
+                        AndroidId androidIdAnnotation = (AndroidId) annotation;
+                        if (platform.equals(Platform.ANDROID)) {
+                            if (!androidIdAnnotation.value().isEmpty()) {
+                                field.set(page, By.id(androidIdAnnotation.value()));
+                            } else if (!androidIdAnnotation.xpath().isEmpty()) {
+                                field.set(page, By.xpath(androidIdAnnotation.xpath()));
+                            }
                         }
                     }
-                }
-                if (annotation instanceof IOSId) {
-                    IOSId iOSIdAnnotation = (IOSId) annotation;
-                    if (platform.equals(Platform.IOS)) {
-                        if (!iOSIdAnnotation.value().isEmpty()) {
-                            field.set(page, By.id(iOSIdAnnotation.value()));
-                        } else if (!iOSIdAnnotation.xpath().isEmpty()) {
-                            field.set(page, By.xpath(iOSIdAnnotation.xpath()));
+                    if (annotation instanceof IOSId) {
+                        IOSId iOSIdAnnotation = (IOSId) annotation;
+                        if (platform.equals(Platform.IOS)) {
+                            if (!iOSIdAnnotation.value().isEmpty()) {
+                                field.set(page, By.id(iOSIdAnnotation.value()));
+                            } else if (!iOSIdAnnotation.xpath().isEmpty()) {
+                                field.set(page, By.xpath(iOSIdAnnotation.xpath()));
 
+                            }
                         }
                     }
                 }
             }
-        }
+            current = current.getSuperclass();
+        } while (current != null);
+
     }
+
 }
+
