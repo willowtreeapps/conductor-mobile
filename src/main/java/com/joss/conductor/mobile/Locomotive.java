0b364176392a4d916fee06bd0d4791216d52bc70
Jared1048 <jared.owen@willowtreeapps.com>
2018-05-24 09:06:53 -0400

    increasing screen margin

--- src/main/java/com/joss/conductor/mobile/Locomotive.java
+++ src/main/java/com/joss/conductor/mobile/Locomotive.java
@@ -532,7 +532,7 @@ public class Locomotive extends Watchman implements Conductor<Locomotive>, Sauce
     private Locomotive performCornerSwipe(ScreenCorner corner, SwipeElementDirection direction, float percentage, int duration) {
         Dimension screen = getAppiumDriver().manage().window().getSize();
 
-         final int SCREEN_MARGIN = 30;
+         final int SCREEN_MARGIN = 130;
 
         Point from;
         if(corner != null) {
